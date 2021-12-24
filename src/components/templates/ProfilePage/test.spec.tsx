d3843982abe144a5e402e50025d5ccc6e1907c22
Alisson Leal <alissonsleal@hotmail.com>
2021-12-24 01:41:23 -0300

    test: use new ProfilePage template in videos and deletedvods

---
+++ src/components/templates/ProfilePage/test.spec.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import { StreamerInformation } from '~/@types/StreamerInformation'
+import { darkTheme } from '~/layout/theme'
+import ProfilePage from '.'
+import 'jest-styled-components'
+import { useRouter } from '../../../../node_modules/next/router'
+
+const mockedStreamerInformation: StreamerInformation = {
+  displayName: 'xQcOw',
+  name: 'xqcow',
+  logo: 'https://static-cdn.jtvnw.net/jtv_user_pictures/xqcow-profile_image-9298dca608632101-150x150.jpeg',
+  followers: 121212,
+  description: 'mr. cow is a streamer',
+}
+
+jest.mock('../../../../node_modules/next/router')
+
+describe('ProfilePage component', () => {
+  it('should render correctly with subonly pathname', () => {
+    // @ts-ignore
+    useRouter.mockImplementation(() => ({
+      pathname: '/videos/xqcow',
+    }))
+
+    const { container } = render(
+      <ThemeProvider theme={darkTheme}>
+        <ProfilePage streamerInformation={mockedStreamerInformation}>
+          Inside content
+        </ProfilePage>
+      </ThemeProvider>,
+    )
+
+    expect(container).toBeInTheDocument()
+    expect(screen.getByText('Inside content')).toBeInTheDocument()
+    expect(screen.getByText('Sub only VODs')).toBeInTheDocument()
+    expect(screen.getByText('Deleted VODs')).toBeInTheDocument()
+  })
+
+  it('should render correctly with deletedvods pathname', () => {
+    // @ts-ignore
+    useRouter.mockImplementation(() => ({
+      pathname: '/deletedvods/xqcow',
+    }))
+
+    const { container } = render(
+      <ThemeProvider theme={darkTheme}>
+        <ProfilePage streamerInformation={mockedStreamerInformation}>
+          Inside content
+        </ProfilePage>
+      </ThemeProvider>,
+    )
+
+    expect(container).toBeInTheDocument()
+    expect(screen.getByText('Inside content')).toBeInTheDocument()
+    expect(screen.getByText('Sub only VODs')).toBeInTheDocument()
+    expect(screen.getByText('Deleted VODs')).toBeInTheDocument()
+  })
+})
