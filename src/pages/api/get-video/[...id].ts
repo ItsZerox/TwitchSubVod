6e4243d1827bf4c45196e7dd9b405184b37b891e
Alisson Leal <alissonsleal@hotmail.com>
2021-12-26 17:40:50 -0300

    feat: create endpoint to get hls video object

---
+++ src/pages/api/get-video/[...id].ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ITwitchVideo } from '~/@types/ITwitchVideo'
+import { VideoUrl } from '~/@types/VideoUrl'
+import api from '~/services/config'
+import { getUrlsFromVideo } from '~/utils/getUrlFromVideo/getUrlsFromVideo'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id[0] as string
+  console.log(id)
+  const { data } = await api.get<ITwitchVideo>(
+    `/videos/${id.replace('.m3u8', '')}`,
+  )
+  const urls = getUrlsFromVideo(data)
+
+  res.setHeader('Content-Type', 'binary/octet-stream')
+
+  const formatStreamInformation = (urlInformation: VideoUrl, index: number) => {
+    const [width, height] = urlInformation.resolution.split('x')
+    const widthInt = parseInt(width)
+    const heightInt = parseInt(height)
+    const bandwidth = widthInt * heightInt * index + 1
+
+    return `#EXT-X-STREAM-INF:BANDWIDTH=${bandwidth},RESOLUTION=${urlInformation.resolution}`
+  }
+
+  res.write(`#EXTM3U
+#EXT-X-VERSION:3
+${urls
+  .map(
+    (url, index) => `
+${formatStreamInformation(url, index)}
+${process.env.NEXT_PUBLIC_CORS + url.url}
+`,
+  )
+  .join('\n')}
+`)
+
+  res.end()
+}
+
+export default handler
