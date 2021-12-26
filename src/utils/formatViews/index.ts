1654140abf9f0afccb71b6d2b6e6582814749652
Alisson Leal <alissonsleal@hotmail.com>
2021-12-25 21:42:00 -0300

    feat: create formatViews util

---
+++ src/utils/formatViews/index.ts
@@ -0,0 +1,24 @@
+const formatViews = (views: number, viewText?: string) => {
+  const viewTextFinal = viewText ? ` ${viewText}` : ''
+
+  if (views < 1000) {
+    return `${views}${viewTextFinal}`
+  } else if (views >= 1000 && views < 1000000) {
+    const thousands = views / 1000
+    const roundedThousands = Math.round(thousands * 10) / 10
+
+    return `${roundedThousands}k${viewTextFinal}`
+  } else if (views >= 1000000 && views < 1000000000) {
+    const millions = views / 1000000
+    const roundedMillions = Math.round(millions * 10) / 10
+
+    return `${roundedMillions}m${viewTextFinal}`
+  } else if (views >= 1000000000) {
+    const billions = views / 1000000000
+    const roundedBillions = Math.round(billions * 10) / 10
+
+    return `${roundedBillions}b${viewTextFinal}`
+  }
+}
+
+export default formatViews
