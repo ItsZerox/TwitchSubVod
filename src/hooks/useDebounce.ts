d75209b00d77af1f7f4cd1d7bc68f4def839b07a
Alisson Leal <alissonsleal@hotmail.com>
2021-12-27 01:43:51 -0300

    feat: create useDebounce custom hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,34 @@
+// Source: https://usehooks-ts.com/react-hook/use-debounce
+
+import { useEffect, useState } from 'react'
+
+/**
+ * @example
+ *const [value, setValue] = useState<string>('')
+  const debouncedValue = useDebounce<string>(value, 500)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  // Fetch API (optional)
+  useEffect(() => {
+    // Do fetch here...
+    // Triggers when "debouncedValue" changes
+  }, [debouncedValue])
+ */
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
+
+export default useDebounce
