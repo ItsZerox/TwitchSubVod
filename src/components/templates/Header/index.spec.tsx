5eff5ac8a2cd6de42159fa5426c16c94d1c93c21
Alisson Leal <alissonsleal@hotmail.com>
2021-12-27 13:25:52 -0300

    test: mock useRouter in header

--- src/components/templates/Header/index.spec.tsx
+++ src/components/templates/Header/index.spec.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from '~/lib/testUtils'
 import Header from '.'
+import { useRouter } from '../../../../node_modules/next/router'
+
+jest.mock('next/router')
 
 describe('Header component', () => {
   it('should render correctly', () => {
+    ;(useRouter as jest.Mock).mockReturnValue({
+      asPath: '/',
+    })
+
     render(<Header />)
 
     const hamburgerMenu = screen.getByRole('button', { name: 'Open menu' })
