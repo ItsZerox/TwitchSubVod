250ae33e269dd7ee6edd9168376a0e15c444d998
Alisson Leal <alissonsleal@hotmail.com>
2021-12-31 13:08:51 -0300

    fix: return epoch as number

--- src/utils/stringToEpoch/index.ts
+++ src/utils/stringToEpoch/index.ts
@@ -1,9 +1,10 @@
-export const stringToEpoch = (date: string) => {
+export const stringToEpoch = (date: string): number => {
   let epochMs = Math.floor(new Date(date).getTime())
 
   if (isNaN(epochMs)) {
     throw new Error('Invalid date')
   }
 
-  return epochMs.toString().substring(0, 10)
+  // return epoch in seconds
+  return epochMs / 1000
 }
