74fcb887b7e8745e5d8b92e5217a57c93508796e
Alisson Leal <alissonsleal@hotmail.com>
2022-01-03 00:22:46 -0300

    feat: get data from mongodb for deletedVods in /video

--- src/pages/video/[vod].tsx
+++ src/pages/video/[vod].tsx
@@ -1,14 +1,13 @@
-import type {
-  GetStaticPropsContext,
-  InferGetStaticPropsType,
-  NextPage,
-} from 'next'
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import { IVideo } from '~/@types/IVideo'
 import { videoAdapter } from '~/adapters/videoAdapter'
 import Video from '~/components/screens/Video/[vod]'
 import revalidate from '~/constants/revalidate'
+import connectDB from '~/lib/mongodb/mongodbConnect'
+import deletedVodsV2 from '~/lib/mongodb/models/deletedVodsV2'
 import { getTopVideos } from '~/services/api/getTopVideos'
 import { getVideo } from '~/services/api/getVideo'
+import { IDeletedVodSchema } from '~/@types/DeletedVodSchema'
 
 export async function getStaticPaths() {
   return {
@@ -33,9 +32,49 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   ]).catch(() => [null, null])
 
   if (!videoData || !relatedVideosData) {
+    await connectDB()
+    const deletedVodData: IDeletedVodSchema = await deletedVodsV2.findOne({
+      streamId: context.params.vod as string,
+    })
+
+    if (!deletedVodData) {
+      return {
+        notFound: true,
+      } as const
+    }
+
+    const video: IVideo = {
+      streamerInformation: {
+        name: deletedVodData.name,
+        displayName: deletedVodData.displayName,
+        logo: deletedVodData.logo,
+        description: '',
+        followers: 0,
+      },
+      vodInformation: {
+        id: deletedVodData.streamId.toString(),
+        date: deletedVodData.streamDate,
+        duration: 1,
+        thumbnail: '',
+        title: '',
+        viewCount: 0,
+      },
+    }
+
+    // todo: have better error handling than requesting data again https://youtu.be/d77uAqi2Ij0?t=18
+    const newRelatedVideosData = await getTopVideos({
+      language: context.locale,
+      limit: 32,
+    })
+
+    const relatedVideos: IVideo[] = newRelatedVideosData.vods.map(videoAdapter)
+
     return {
-      notFound: true,
-    } as const
+      props: {
+        video,
+        relatedVideos: relatedVideos,
+      },
+    }
   }
 
   const video: IVideo = videoAdapter(videoData)
