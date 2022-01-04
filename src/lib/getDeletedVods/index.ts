a660521673dc4be4b3a6587acc598aa3755cddd4
Alisson Leal <alissonsleal@hotmail.com>
2022-01-04 00:05:05 -0300

    chore:  add debug logs

--- src/lib/getDeletedVods/index.ts
+++ src/lib/getDeletedVods/index.ts
@@ -10,11 +10,14 @@ export const getDeletedVods = async (
   username: string,
   range?: number,
 ): Promise<IDeletedVods[]> => {
+  console.log('1')
   const { data } = await scraper.get(
     `${process.env.DELETED_VODS_HOST}${username}`,
   )
 
+  console.log('2')
   const streamerId = getStreamerId(data)
+  console.log('3')
 
   const allVodsResponse = await scraper.get(
     `${process.env.DELETED_VODS}${range || 2}/${streamerId}${
@@ -22,6 +25,7 @@ export const getDeletedVods = async (
     }`,
   )
 
+  console.log('4')
   const allVods: IExternalDeletedVodsApi[] = allVodsResponse.data.data
 
   if (!allVods.length) {
@@ -30,6 +34,7 @@ export const getDeletedVods = async (
 
   const streamsObject = await deletedVodsApiAdapter(allVods)
 
+  console.log('5')
   const streamsPromise = streamsObject.map(async (stream) => {
     const streamUrls = await getDeletedVodUrls({
       streamerName: stream.name,
@@ -43,8 +48,10 @@ export const getDeletedVods = async (
     }
   })
 
+  console.log('6')
   const uploadedVodsPromise = uploadToDatabase(streamsObject)
 
+  console.log('7')
   const [streams] = await Promise.all([
     Promise.all(streamsPromise),
     uploadedVodsPromise,
