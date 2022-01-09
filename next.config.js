6890cba674bbf59e404e591cf2f6bccaa0089f40
Alisson Leal <alissonsleal@hotmail.com>
2022-01-08 23:12:01 -0300

    chore: add redirects for old site

--- next.config.js
+++ next.config.js
@@ -11,4 +11,23 @@ module.exports = withTM({
     locales: ['en', 'es', 'fr', 'pt'],
     defaultLocale: 'en',
   },
+  async redirects() {
+    return [
+      {
+        source: '/deletedclips',
+        destination: 'https://old.pogu.live/deletedclips',
+        permanent: true,
+      },
+      {
+        source: '/deletedvods',
+        destination: 'https://old.pogu.live/deletedvods',
+        permanent: true,
+      },
+      {
+        source: '/downloadclip',
+        destination: 'https://old.pogu.live/downloadclip',
+        permanent: false,
+      },
+    ]
+  },
 })
