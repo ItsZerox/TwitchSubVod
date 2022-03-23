887d4c993cd204310a60b0f57248379f17e02afa
Alisson Leal <alissonsleal@hotmail.com>
2022-03-23 20:15:58 -0300

    feat: create getComments service

---
+++ src/services/chat/getComments.ts
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction } from 'react'
+import { ITwitchVideoComment } from '~/@types/ITwitchVideoComment'
+import { commentsAdapter, IComment } from '~/adapters/commentsAdapter'
+import api from '../config'
+
+interface IGetComments {
+  vodId: string
+  currentVideoTime: number
+  setNewComments: Dispatch<SetStateAction<IComment[]>>
+}
+
+export const getComments = async ({
+  vodId,
+  currentVideoTime,
+  setNewComments,
+}: IGetComments) => {
+  const params = {
+    content_offset_seconds: currentVideoTime,
+  }
+
+  const { data } = await api.get<ITwitchVideoComment>(
+    `/videos/${vodId}/comments`,
+    {
+      params,
+    },
+  )
+
+  const adaptedComments = commentsAdapter(data.comments)
+
+  setNewComments((oldNewComments) =>
+    Array.from(new Set([...oldNewComments, ...adaptedComments])),
+  )
+}
