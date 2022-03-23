98f87e67fa14fdf94d4bcf5d1c8732634ece3fe3
Alisson Leal <alissonsleal@hotmail.com>
2022-03-23 00:53:39 -0300

    fix: omit zero in hour in convertToHourMinuteSecond util

--- src/utils/convertToHourMinuteSecond/index.ts
+++ src/utils/convertToHourMinuteSecond/index.ts
@@ -3,7 +3,7 @@ export const convertToHourMinuteSecond = (seconds: number) => {
   const minute = Math.floor((seconds % 3600) / 60)
   const second = Math.floor((seconds % 3600) % 60)
 
-  const prettyHour = hour < 10 ? `0${hour}` : hour
+  const prettyHour = hour
   const prettyMinute = minute < 10 ? `0${minute}` : minute
   const prettySecond = second < 10 ? `0${second}` : second
 
