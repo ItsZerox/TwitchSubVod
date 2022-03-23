6f64402c2e62de118936784915e4eb9a1a96b482
Alisson Leal <alissonsleal@hotmail.com>
2022-03-23 20:53:08 -0300

    feat: add emotes constants

---
+++ src/constants/emotes.ts
@@ -0,0 +1,23 @@
+// CDNs
+const TWITCH_CDN = 'https://static-cdn.jtvnw.net'
+const FFZ_EMOTE_CDN = 'https://cdn.frankerfacez.com/emote'
+const BTTV_EMOTE_CDN = 'https://archive.overpowered.tv/bttv'
+const SEVENTV_EMOTE_CDN = 'https://cdn.7tv.app/emote'
+
+// APIs
+const TWITCH_BADGES_API =
+  'https://badges.twitch.tv/v1/badges/global/display?language=en'
+const FFZ_EMOTE_API = 'https://api.frankerfacez.com/v1'
+const BTTV_EMOTE_API = 'https://api.betterttv.net/3'
+const SEVENTV_EMOTE_API = 'https://api.7tv.app/v2'
+
+export {
+  TWITCH_CDN,
+  FFZ_EMOTE_CDN,
+  BTTV_EMOTE_CDN,
+  SEVENTV_EMOTE_CDN,
+  TWITCH_BADGES_API,
+  FFZ_EMOTE_API,
+  BTTV_EMOTE_API,
+  SEVENTV_EMOTE_API,
+}
