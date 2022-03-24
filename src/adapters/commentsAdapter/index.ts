98dad3f231838215425a84e8b4abe209e926941a
Alisson Leal <alissonsleal@hotmail.com>
2022-03-24 02:24:50 -0300

    chore: add badges to comments adapter

--- src/adapters/commentsAdapter/index.ts
+++ src/adapters/commentsAdapter/index.ts
@@ -1,4 +1,8 @@
-import { Fragment, ITwitchComment } from '~/@types/ITwitchVideoComment'
+import {
+  Fragment,
+  ITwitchComment,
+  UserBadge,
+} from '~/@types/ITwitchVideoComment'
 
 export interface IComment {
   _id: string
@@ -6,6 +10,7 @@ export interface IComment {
   fragments: Fragment[]
   displayName: string
   userColor: string
+  badges: UserBadge[]
 }
 
 export const commentsAdapter = (comment: ITwitchComment[]): IComment[] => {
@@ -16,6 +21,7 @@ export const commentsAdapter = (comment: ITwitchComment[]): IComment[] => {
       fragments: comment.message.fragments,
       displayName: comment.commenter.display_name,
       userColor: comment.message.user_color,
+      badges: comment.message.user_badges,
     }
   })
 }
