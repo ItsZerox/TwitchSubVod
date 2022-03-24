5ef39d03da3b4f27084ada5b69b6d565024d5898
Alisson Leal <alissonsleal@hotmail.com>
2022-03-24 01:48:39 -0300

    fix: make chatbox 100vh

--- src/components/molecules/ChatBox/styles.ts
+++ src/components/molecules/ChatBox/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ChatBoxContainer = styled.ul`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   max-height: calc(100vh - 100px);
   overflow-y: scroll;
   background: ${({ theme }) => theme.colors.grey1000};
