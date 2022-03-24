9d71fc3d5c3b769ec53412a24e7e4f9935ee2805
Alisson Leal <alissonsleal@hotmail.com>
2022-03-23 22:55:40 -0300

    feat: create message styles

--- src/components/atoms/ChatMessage/styles.ts
+++ src/components/atoms/ChatMessage/styles.ts
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 export const ChatMessageContainer = styled.li`
   color: white;
   list-style: none;
-  padding: 5px 0 5px 10px;
+  padding: 3px 0 3px 10px;
   width: 100%;
   font-size: 13px;
+  line-height: 1.3rem;
 `
 
 export const ChatMessageTimestamp = styled.span`
@@ -20,4 +21,8 @@ export const ChatMessageChatter = styled.span<{ textColor?: string }>`
 
 export const ChatMessageBody = styled.span`
   width: 100%;
+`
+export const Emote = styled.img`
+  vertical-align: middle;
+  margin: -5rem 0;
 `
